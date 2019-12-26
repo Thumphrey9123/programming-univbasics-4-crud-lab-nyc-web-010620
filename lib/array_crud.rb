0bd5c4112531cfe2611aace0e074af91d1db116e
Thomas Humphrey <thumphrey9123@gmail.com>
2019-12-26 21:39:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  arr = []
+  arr = [1,2,3,4,5]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.index(index_number)
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
@@ -31,7 +31,7 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array[(array.length-1)]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
